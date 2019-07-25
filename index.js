5aace98bacc9f2efca56eef5ded53c22fb091be7
Maksim Pesetski <shanuyzelen@gmail.com>
2019-07-25 18:39:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+const takeANumber = (line, name) => {
+
+  line.push(name);
+
+  return `Welcome, ${name}. You are number ${line.indexOf(name) + 1} in line.`
+}
+
+const nowServing = (line) => {
+
+  if (!line.length) return "There is nobody waiting to be served!"
+
+  let first = line.shift();
+
+  return `Currently serving ${first}.`
+}
+
+
+const currentLine = (line) => {
+  let str = 'The line is currently:';
+
+
+}
